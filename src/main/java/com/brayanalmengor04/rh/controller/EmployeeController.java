623914388a3d3889873a4b861492f73ab7e92892
Brayan Almengor <brayanalmengor300@gmail.com>
2025-02-23 21:04:12 -0500

    creation endpoints rh practice system react $ spring boot 3

--- src/main/java/com/brayanalmengor04/rh/controller/EmployeeController.java
+++ src/main/java/com/brayanalmengor04/rh/controller/EmployeeController.java
@@ -1,14 +1,18 @@
 package com.brayanalmengor04.rh.controller;
 
 import com.brayanalmengor04.rh.entity.Employee;
+import com.brayanalmengor04.rh.exception.ResourceNotFound;
 import com.brayanalmengor04.rh.service.EmployeeService;
 import com.brayanalmengor04.rh.service.IEmployeService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("rh-app")
@@ -23,7 +27,7 @@ public class EmployeeController {
     private IEmployeService employeService;
 
     // Solicitar Toda la lita de empleados
-    @GetMapping("/employees")
+    @GetMapping("/employee")
     public List<Employee> getEmployees() {
         var employees = employeService.listEmployees();
         employees.forEach(employee ->
@@ -38,5 +42,40 @@ public class EmployeeController {
         return employeService.saveEmployee(employee);
     }
 
+    // Editar empleados para frontend esto es para mostrar los datos que vamos editar
 
+    @GetMapping("/employee/{id}")
+    public ResponseEntity<Employee> getEmployeeById(@PathVariable Integer id) {
+        Employee employee = employeService.searchEmployeeById(id);
+        if (employee == null) throw new ResourceNotFound("Employee not found");
+        return ResponseEntity.ok(employee);
+    }
+
+    // Metodo para editar (formulario)
+    @PutMapping("/employee/{id}")
+    public ResponseEntity<Employee> updateEmployee(@PathVariable Integer id
+            , @RequestBody Employee employee) {
+
+        Employee updatedEmployee = employeService.searchEmployeeById(id);
+        if (updatedEmployee == null) throw new ResourceNotFound("Employee not found");
+        updatedEmployee.setName(employee.getName());
+        updatedEmployee.setSalary(employee.getSalary());
+        updatedEmployee.setDepartment(employee.getDepartment());
+        employeService.saveEmployee(updatedEmployee);
+        return ResponseEntity.ok(updatedEmployee);
+    }
+
+    // Metodo para eliminar
+
+    @DeleteMapping("/employee/{id}")
+    public ResponseEntity<Map<String,Boolean>> deleteEmployee
+            (@PathVariable Integer id) {
+        Employee employee = employeService.searchEmployeeById(id);
+        if (employee == null) throw new ResourceNotFound("Employee not found");
+        employeService.deleteEmployee(employee);
+        // Json {"eliminado":"true"}
+        Map<String,Boolean> response = new HashMap<>();
+        response.put("deleted", Boolean.TRUE);
+        return ResponseEntity.ok(response);
+    }
 }
